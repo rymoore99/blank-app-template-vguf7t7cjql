68d83503edda8af4cd6310b12fc9b4cb714ce5c1
Ryan Moore <rymoore99@users.noreply.github.com>
2024-07-09 15:26:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ if len(pn) > 0:
     df = conn.query(f'select *, SIMILARITY(search_string, \'{card}\') as sim from "ACMETRADING_checklist_pre1970" where SIMILARITY(search_string, \'{card}\') > 0.1 ORDER BY sim DESC limit 20', ttl="10m")
 
     # Print results.
-    st.dataframe(df[['search_string', 'parallel', 'sim', 'url_full']], use_container_width=True, hide_index=True,
+    st.dataframe(df[['search_string', 'variation', 'sim', 'url_full']], use_container_width=True, hide_index=True,
     column_config={
         "search_string": "Card",
         "parallel": 'Parallel',
